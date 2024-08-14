a7ea175049a2efe79da615fa46827d5af1a7c36d
Bethelhem Daniel <82999200+Bethel-21@users.noreply.github.com>
2024-08-14 13:31:10 +0300

    Drop-down contents are linked

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ function updateTimeDate() {
   let timeElement = document.querySelector("#addis-ababa");
   let addisTime = moment
     .tz("Africa/Addis_Ababa")
-    .format("h:mm:ss [<small>AM</small>]");
+    .format("h:mm:ss A");
   timeElement.innerHTML = addisTime;
 
   let dateElementNext = document.querySelector(".london-date");
@@ -16,9 +16,33 @@ function updateTimeDate() {
   let timeElementNext = document.querySelector("#london");
   let londonTime = moment
     .tz("Europe/London")
-    .format("h:mm:ss [<small>AM</small>]");
+    .format("h:mm:ss A");
   timeElementNext.innerHTML = londonTime;
 }
 
 updateTimeDate();
 setInterval(updateTimeDate, 1000);
+
+function updateCity(event) {
+  let cityTimeZone = event.target.value;
+  let cityTime = moment().tz(cityTimeZone);
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1]
+
+  let updatePlace = document.querySelector(".details");
+  updatePlace.innerHTML = `<div class="detail-place">
+          <div>
+            <h1>${cityName}</h1>
+            <p class = "change-color">${cityTime.format("MMMM Do YYYY")}</p>
+          </div>
+          <div >
+            <h1> ${cityTime.format(
+              "h:mm:ss"
+            )} <small>${cityTime.format("A")}</small>
+              
+            </h1>
+          </div>
+        </div>`;
+}
+
+let displayElement = document.querySelector("#drop-down-elements");
+displayElement.addEventListener("change", updateCity);
